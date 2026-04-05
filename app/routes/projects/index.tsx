bbb7cfdb56706bf6ee8eac7474fb180734538831
htethlaingwin <htethlaingwin.bsn29@gmail.com>
2026-04-05 21:31:54 +0630

    Showing Project Card on Page

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,5 +1,6 @@
 import type { Project } from '~/types';
 import type { Route } from './+types/index';
+import ProjectCard from '~/components/ProjectCard';
 
 export async function loader({ request }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
   const res = await fetch('http://localhost:8000/projects');
@@ -9,11 +10,16 @@ export async function loader({ request }: Route.LoaderArgs): Promise<{ projects:
 }
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
   const { projects } = loaderData as { projects: Project[] };
-  console.log(projects);
 
   return (
     <>
       <h2 className="mb-8 text-3xl font-bold text-white">🚀 Projects</h2>
+
+      <div className="grid gap-6 sm:grid-cols-2">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
     </>
   );
 };
