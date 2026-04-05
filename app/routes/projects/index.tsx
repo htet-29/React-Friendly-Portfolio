d2d3c9a3c58d5c544f42ef77c69a57f39ee400c7
htethlaingwin <htethlaingwin.bsn29@gmail.com>
2026-04-05 20:12:56 +0630

    Loader for Data Fetching

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,4 +1,15 @@
-const ProjectsPage = () => {
+import type { Route } from './+types/index';
+
+export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+  const res = await fetch('http://localhost:8000/projects');
+  const data = await res.json();
+
+  return { projects: data };
+}
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData;
+  console.log(projects);
+
   return (
     <>
       <h2 className="mb-8 text-3xl font-bold text-white">🚀 Projects</h2>
