ca546c2dd4a48e695de75a55c42ca26fca9886ea
htethlaingwin <htethlaingwin.bsn29@gmail.com>
2026-04-09 20:50:28 +0630

    Post Meta

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,4 +1,21 @@
-const Blog = () => {
+import type { PostMeta } from '~/types';
+import type { Route } from './+types/index';
+import { Link } from 'react-router';
+
+export const loader = async ({ request }: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> => {
+  const url = new URL('/posts-meta.json', request.url);
+  const res = await fetch(url.href);
+
+  if (!res.ok) throw new Error('Failed to fetch data');
+
+  const data = await res.json();
+
+  return { posts: data };
+};
+
+const Blog = ({ loaderData }: Route.ComponentProps) => {
+  const { posts } = loaderData;
+  console.log(posts);
   return (
     <>
       <h2 className="mb-8 text-3xl font-bold text-white">📑 Blogs</h2>
